9778f487f7bcc888c8ef4649155545671918d989
KellyPared <kellschu333@gmail.com>
2022-06-30 10:21:19 -0400

    This is a refactored version of the original code.

--- quiz.py
+++ quiz.py
@@ -31,17 +31,15 @@ QUESTIONS = {
     ],
 }
 
-num_questions = min(NUM_QUESTIONS_PER_QUIZ, len(QUESTIONS))
-'''random sample - randomize the order of your questions and the order of the answer alternatives'''
+def prepare_questions(questions, num_questions):
+    '''deals with general questions and number of questions parameters'''
+    num_questions = min(num_questions, len(questions))
+    return random.sample(list(questions.items()),k=num_questions)
 
-questions = random.sample(list(QUESTIONS.items()),k=num_questions)
 
-num_correct = 0 # counter
-for num, (question, alternatives) in enumerate(questions, start=1): #for key,value in dictionary with enumerate
-    print(f" \nQuestion {num}:")
+def get_answer(question, alternatives):
+    '''this function accepts a question text and list alternativs'''
     print(f"{question}?")
-    correct_answer = alternatives[0] # the correct answer is assigned the first value in the list
-
     labeled_alternatives = dict(zip(ascii_lowercase, random.sample(alternatives, k=len(alternatives)))) # sort assorted list
     for label, alternatives in labeled_alternatives.items():
         print(f"   {label}) {alternatives}")
@@ -49,12 +47,31 @@ for num, (question, alternatives) in enumerate(questions, start=1): #for key,val
     while (answer_label := input("\nChoice? ")) not in labeled_alternatives: #walrus operator checks if valid option
         print(f"Please answer one of the {', '.join(labeled_alternatives)}")
 
-    answer = labeled_alternatives[answer_label]
+    return labeled_alternatives[answer_label]
+
+
+def ask_question(question, alternatives):
+    correct_answer = alternatives[0] # the correct answer is assigned the first value in the list
+    ordered_alternatives = random.sample(alternatives, k=len(alternatives))
+
+    answer = get_answer(question,ordered_alternatives) # handles all details about getting an answer from the user
     if answer == correct_answer:
-        num_correct +=1
+        #num_correct +=1
         print("⭐ Correct! ⭐ ")
+        return 1
     else:
         print(f"The answer is {correct_answer!r}, not {answer!r}")
+        return 0
 
-print(f"\nYou got {num_correct} correct out of {num} questions")
+def run_quiz():
+    questions = prepare_questions(QUESTIONS, num_questions=NUM_QUESTIONS_PER_QUIZ)
 
+    num_correct = 0 # counter
+    for num, (question, alternatives) in enumerate(questions, start=1): #for key,value in dictionary with enumerate
+        print(f" \nQuestion {num}:")
+        num_correct += ask_question(question, alternatives)
+
+    print(f"\nYou got {num_correct} correct out of {num} questions")
+
+if __name__ =="__main__":
+    run_quiz()
---
+++ run_quiz.py
@@ -0,0 +1,11 @@
+import random
+from string import ascii_lowercase
+
+#dict(zip(string.ascii_lowercase,[ "1781", "1771", "1871", "1881"],))
+
+
+
+
+questions = random.sample(list(QUESTIONS.items()),k=num_questions)
+
+
